48ec80657d25981648e2f8ead655dd19b2a4abee
Lou <l_saint_andre@hotmail.com>
2022-07-22 16:56:38 -0400

    (someJS)

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ app.init = () => {
     console.log("hello")
 }
 
+let filler = $(`.filler`);
+
+console.log(filler)
+
+if (filler.attr(`width`, `>100px`) ) {
+    filler.attr(`background-color`, `blue`)
+}
 
 
 
