b895add8d14aaeb915b1ceb35ee4980de1fc58dd
Lou <l_saint_andre@hotmail.com>
2022-07-23 13:23:23 -0400

    todayWork

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let filler = $(`.filler`);
 
 console.log(filler)
 
-if (filler.attr(`width:`, `>= 100px`) ) {
+if (filler.attr(`width:`, `>= 100px`, true) ) {
     filler.attr(`background-color`, `blue`)
 }
 
